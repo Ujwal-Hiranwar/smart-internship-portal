a66f2def71b27a1f9c9336eef7e479ce5527f0f9
Ujwal-Hiranwar <ujjwalhiranwar15@gmail.com>
2026-02-24 16:29:13 +0000

    Initial commit

---
+++ smartinternship-backend/src/test/java/com/example/smartinternship/SmartinternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartinternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
